d92023c0a468bd483d92c8dc823642be5b4616bf
tuantuana <91653958+tuantuana@users.noreply.github.com>
2026-01-09 10:26:34 +0000

    newid

--- config.js
+++ config.js
@@ -10,7 +10,13 @@ const to = from; // Thời gian hiện tại
 module.exports = {
     TELEGRAM_BOT_TOKEN: '8307422056:AAH9qbB5wjN7hJnB5ZeplOGPWITYS9bQOwg',
     // TELEGRAM_BOT_TOKEN: '7640879888:AAGG-YwTdCiAjimmnMZnAXDqYeNYmn78OsI',
-    TELEGRAM_CHAT_ID: '5710130520',
+     // 👇 đổi từ CHAT_ID sang CHAT_IDS
+    TELEGRAM_CHAT_IDS: [
+        '5710130520',
+        '5956854458',
+        // '-100xxxxxxxxxx', // group nếu có
+    ],
+
     API_KEY: 'be5a6dbf-43bc-4afb-86d6-2421da6b287e',
     from,
     to
